e441a3d48d94ee607b969d1e74f0be3ec0cdfdfc
@hexlet/cli <support@hexlet.io>
2024-12-26 23:57:51 +0300

    submit rails-basics-ru/static-pages

--- rails-basics-ru/static-pages/config/routes.rb
+++ rails-basics-ru/static-pages/config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   # BEGIN
   root 'Home#index'
   get 'Pages/show'
-  get 'Pages/about'
   # END
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
