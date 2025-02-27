6ba7461307457dc342a4b86da9cf7332ac5d6c88
@hexlet/cli <support@hexlet.io>
2025-02-27 20:29:43 +0300

    submit rails-real-ru/emails

---
+++ rails-real-ru/emails/app/mailers/user_mailer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class UserMailer < ApplicationMailer
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.account_activation.subject
+  #
+  def account_activation
+    # BEGIN
+    @user = params[:user]
+
+    mail(
+      subject: t('user_mailer.account_activation.subject'),
+      to: @user.email
+    )
+    # END
+  end
+end
